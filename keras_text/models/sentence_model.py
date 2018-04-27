142016a9cd9f3a41c02e9b657be490206f07285f
Denis Zuenko <neron.v2@gmail.com>
2018-04-27 21:20:38 +0300

    Update sentence_model.py
    Fixed - 'dict_values' object does not support indexing

--- keras_text/models/sentence_model.py
+++ keras_text/models/sentence_model.py
@@ -35,7 +35,7 @@ class SentenceModelFactory(object):
 
         if embedding_type is not None:
             self.embeddings_index = get_embeddings_index(embedding_type)
-            self.embedding_dims = self.embeddings_index.values()[0].shape[-1]
+            self.embedding_dims = list(self.embeddings_index.values())[0].shape[-1]
         else:
             self.embeddings_index = None
             self.embedding_dims = embedding_dims
@@ -95,3 +95,4 @@ class SentenceModelFactory(object):
 
         x = Dense(self.num_classes, activation=output_activation)(x)
         return Model(doc_input, x)
+
