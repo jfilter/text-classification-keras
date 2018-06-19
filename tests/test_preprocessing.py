62812b3d4d1571a9c5ad53e8f7821ce36468b91a
Johannes Filter <hi@jfilter.de>
2018-06-20 01:15:05 +0200

    fix tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import keras
 import pytest
 
@@ -8,19 +10,19 @@ from keras_text.processing import WordTokenizer, pad_sequences
 def test_pre(tmpdir):
     tokenizer = WordTokenizer()
 
-    X = ['hello', 'world']
-    y = [0, 1]
+    X = ['hello', 'world', 'welcome', 'earth']
+    y = [0, 1, 0, 1]
 
     tokenizer.build_vocab(X)
 
-    assert len(tokenizer.token_index) == 2
+    assert len(tokenizer.token_index) == 4
 
     X_enc = tokenizer.encode_texts(X)
     X_fin = pad_sequences(X_enc, max_tokens=50)
     y_fin = keras.utils.to_categorical(y, num_classes=2)
 
     ds = Dataset(X_fin, y_fin, tokenizer=tokenizer)
-    ds.update_test_indices(test_size=0.1)
+    ds.update_test_indices(test_size=0.5)
 
     path = tmpdir.mkdir("data").join("test")
 
