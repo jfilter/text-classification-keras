d8dc09d1792b95be0a9f2389c28da9c215f7d308
Johannes Filter <hi@jfilter.de>
2018-07-01 14:53:59 +0200

    test training

---
+++ tests/train.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+import keras
+import numpy as np
+import pytest
+
+from keras_text.corpus import imdb
+from keras_text.data import Dataset
+from keras_text.models import AttentionRNN, StackedRNN, TokenModelFactory, YoonKimCNN
+from keras_text.processing import WordTokenizer
+
+max_len = 50
+
+def test_train():
+    X, y, _, _ = imdb(10)
+
+    tokenizer = WordTokenizer()
+
+    tokenizer.build_vocab(X)
+
+    # only select top 10k tokens
+    tokenizer.apply_encoding_options(max_tokens=20)
+
+    X_encoded = tokenizer.encode_texts(X)
+    X_padded = tokenizer.pad_sequences(
+        X_encoded, fixed_token_seq_length=max_len)
+
+    y_cat = keras.utils.to_categorical(y, num_classes=2)
+
+    ds = Dataset(X_padded, y_cat, tokenizer=tokenizer)
+
+    X_train, _, y_train, _ = ds.train_val_split()
+
+
+    # RNN models can use `max_tokens=None` to indicate variable length words per mini-batch.
+    factory = TokenModelFactory(
+        2, ds.tokenizer.token_index, max_tokens=max_len, embedding_type='glove.6B.300d')
+    word_encoder_model = YoonKimCNN(dropout_rate=0)
+    model = factory.build_model(
+        token_encoder_model=word_encoder_model, trainable_embeddings=False)
+
+    sgd = keras.optimizers.SGD(
+        lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+    model.compile(optimizer=sgd,
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    model.fit(X_train, y_train, epochs=1, batch_size=32, validation_split=0.1)
