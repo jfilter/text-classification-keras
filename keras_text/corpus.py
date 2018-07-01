1a0cf56b46985fbaf1edb53c748d47bb5ad8d4b6
Johannes Filter <hi@jfilter.de>
2018-07-01 14:31:04 +0200

    add imdb copurs

---
+++ keras_text/corpus.py
@@ -0,0 +1,62 @@
+import os
+import io
+
+import keras
+
+
+def read_folder(directory):
+    """read text files in directory and returns them as array
+
+    Args:
+        directory: where the text files are
+
+    Returns:
+        Array of text
+    """
+    res = []
+    for filename in os.listdir(directory):
+        with io.open(os.path.join(directory, filename), encoding="utf-8") as f:
+            content = f.read()
+            res.append(content)
+    return res
+
+
+def read_pos_neg_data(path, folder, limit):
+    """returns array with positive and negative examples"""
+    training_pos_path = os.path.join(path, folder, 'pos')
+    training_neg_path = os.path.join(path, folder, 'neg')
+
+    X_pos = read_folder(training_pos_path)
+    X_neg = read_folder(training_neg_path)
+
+    if limit is None:
+        X = X_pos + X_neg
+    else:
+        X = X_pos[:limit] + X_neg[:limit]
+
+    y = [1] * int(len(X) / 2) + [0] * int(len(X) / 2)
+
+    return X, y
+
+
+def imdb(limit=None):
+    """Downloads (and caches) IMDB Moview Reviews. 25k training data, 25k test data
+
+    Args:
+        limit: get only first N items for each class
+
+    Returns:
+        [X_train, y_train, X_test, y_test]
+    """
+
+    movie_review_url = 'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
+
+    # download and extract, thus remove the suffix '.tar.gz'
+    path = keras.utils.get_file(
+        'aclImdb.tar.gz', movie_review_url, extract=True)[:-7]
+
+    X_train, y_train = read_pos_neg_data(path, 'train', limit)
+    X_test, y_test = read_pos_neg_data(path, 'test', limit)
+
+    return X_train, y_train, X_test, y_test
+
