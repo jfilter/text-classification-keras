29d8e28d0263c16e8a5e33909060bfcb36f5d3e2
Johannes Filter <hi@jfilter.de>
2018-07-29 22:25:00 +0200

    add test for word fasttext word embeddings

--- tests/embeddings.py
+++ tests/embeddings.py
@@ -5,3 +5,21 @@ def test_build_index_glove():
     index = get_embeddings_index('glove.6B.50d')
     assert(index['a'] is not None)
     assert(index['and'] is not None)
+
+
+def test_build_index_fasttext_en():
+    index = get_embeddings_index('fasttext.wn.1M.300d')
+    assert(index['a'] is not None)
+    assert(index['and'] is not None)
+
+
+def test_build_index_fasttext_wiki():
+    index = get_embeddings_index('fasttext.wiki.simple')
+    assert(index['a'] is not None)
+    assert(index['and'] is not None)
+
+
+def test_build_index_fasttext_cc():
+    index = get_embeddings_index('fasttext.cc.en')
+    assert(index['a'] is not None)
+    assert(index['and'] is not None)
