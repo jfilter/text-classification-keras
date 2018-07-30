6ce4a9ba875c4d5aff2477e6d88d2706d1819c96
Johannes Filter <hi@jfilter.de>
2018-07-30 22:12:38 +0200

    prepare setup for new release

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-version = '0.1'
+classifiers = [
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 2.7',
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 3 - Alpha'
+]
+
+version = '0.0.0'
 
 setup(name='keras-text',
       version=version,
       description='Text Classification Library for Keras',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Raghavendra Kotikalapudi, Johannes Filter',
       author_email='ragha@outlook.com, hi@jfilter.de',
       url='https://github.com/jfilter/text-classification-keras',
-      download_url='https://github.com/jfilter/text-classification-keras/tarball/{}'.format(
-          version),
       license='MIT',
-      install_requires=['keras>=2.1.2', 'six',
-                        'spacy>=2.0.3', 'scikit-learn', 'joblib', 'jsonpickle', 'numpy'],
-      extras_require={
-          'tests': ['pytest',
-                    'pytest-pep8',
-                    'pytest-xdist',
-                    'pytest-cov'],
-      },
+      install_requires=['keras==2.2.*', 'six==1.10.*',
+                        'scikit-learn==0.19.*', 'joblib==0.12.*',
+                        'jsonpickle==0.9.*', 'numpy==1.5.*'],
+      extras_require={'full': ['spacy==2.0.*', 'deep-plots==0.1.*']},
       include_package_data=True,
+      classifiers=classifiers,
       packages=find_packages())
