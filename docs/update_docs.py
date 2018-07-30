eeaebf980d29c6b0af38c0e010e116b5c3c99a51
Johannes Filter <hi@jfilter.de>
2018-07-30 13:30:02 +0200

    update docs for fork

--- docs/update_docs.py
+++ docs/update_docs.py
@@ -1,18 +1,19 @@
+#!/usr/bin/env python
+
 import shutil
+import subprocess
+import sys
 
 from md_autogen import MarkdownAPIGenerator
 from md_autogen import to_md_file
 
-from keras_text.models import token_model
-from keras_text.models import sentence_model
-from keras_text.models import sequence_encoders
-from keras_text.models import layers
+from keras_text.models import token_model, sentence_model, sequence_encoders, layers
 
-from keras_text import data
-from keras_text import generators
-from keras_text import processing
-from keras_text import sampling
-from keras_text import utils
+from keras_text import data, corpus, embeddings, experiment
+
+from keras_text.utils import format, generators, io, sampling
+
+from keras_text.preprocessing import char_tokenizer, sentence_tokenizer, tokenizer, utils, word_tokenizer
 
 
 def generate_api_docs():
@@ -21,14 +22,12 @@ def generate_api_docs():
         sentence_model,
         sequence_encoders,
         layers,
-        data,
-        generators,
-        processing,
-        sampling,
-        utils
+        data, corpus, embeddings, experiment, format, generators, io, sampling,
+        char_tokenizer, sentence_tokenizer, tokenizer, utils, word_tokenizer
     ]
 
-    md_gen = MarkdownAPIGenerator("keras_text", "https://github.com/raghakot/keras-text/tree/master")
+    md_gen = MarkdownAPIGenerator(
+        "keras_text", "https://github.com/jfilter/text-classification-keras")
     for m in modules:
         md_string = md_gen.module2md(m)
         to_md_file(md_string, m.__name__, "sources")
@@ -47,3 +46,8 @@ if __name__ == "__main__":
     copy_templates()
     update_index_md()
     generate_api_docs()
+    if len(sys.argv) > 1 and sys.argv[1] == '--live':
+        subprocess.run("mkdocs gh-deploy", shell=True, check=True)
+    else:
+        subprocess.run(
+            "mkdocs build && cd site && python3 -m http.server", shell=True, check=True)
