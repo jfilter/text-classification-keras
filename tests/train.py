f0ec25204bfa989a850f4a90644ac819ed22f6c6
Johannes Filter <hi@jfilter.de>
2018-12-06 16:46:47 +0100

    add multi label test training

--- tests/train.py
+++ tests/train.py
@@ -8,7 +8,7 @@ import pytest
 from texcla.corpus import imdb
 from texcla.data import Dataset
 from texcla.models import AttentionRNN, StackedRNN, TokenModelFactory, YoonKimCNN
-from texcla.preprocessing import SpacyTokenizer
+from texcla.preprocessing import SpacyTokenizer, SimpleTokenizer
 from texcla import experiment
 
 max_len = 50
@@ -37,6 +37,36 @@ def test_train():
     # build a model
     model = factory.build_model(
         token_encoder_model=word_encoder_model, trainable_embeddings=False)
+
+    # use experiment.train as wrapper for Keras.fit()
+    experiment.train(x=ds.X, y=ds.y, validation_split=0.1, model=model,
+                     word_encoder_model=word_encoder_model, epochs=1, batch_size=32)
+
+
+def test_train_multi_label():
+    X, y = ['what is up', 'yes yes', 'no no  no'], [
+        ["foo", "bar"], ["foo"], ["bar", "haha"]]
+
+    # use the special tokenizer used for constructing the embeddings
+    tokenizer = SimpleTokenizer()
+
+    # preprocess data (once)
+    experiment.setup_data(X, y, tokenizer, 'data.bin', max_len=100)
+
+    # load data
+    ds = Dataset.load('data.bin')
+
+    # construct base
+    factory = TokenModelFactory(
+        ds.num_classes, ds.tokenizer.token_index, max_tokens=100,
+        embedding_type='glove.6B.50d', embedding_dims=50)
+
+    # choose a model
+    word_encoder_model = YoonKimCNN()
+
+    # build a model
+    model = factory.build_model(
+        token_encoder_model=word_encoder_model, trainable_embeddings=False, output_activation="sigmoid")
 
     # use experiment.train as wrapper for Keras.fit()
     experiment.train(x=ds.X, y=ds.y, validation_split=0.1, model=model,
